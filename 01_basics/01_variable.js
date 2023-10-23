eb8f9bcf5f8bdb4cbc79864030759de8631527cf
Mrityunjay Singh <104375250+Mrityunjay70@users.noreply.github.com>
2023-10-23 05:58:25 +0000

    This code is used to learn variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "mrityunjaysingh@google.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+
+//accountId = 2 //not allowed
+accountEmail = "msingh@mr.com"
+accountPassword = "4356879"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*prefer not to use war
+beacause os issue in block scope and functional scope*/
